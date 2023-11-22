949854e1d50f14f479313f0d648b2acd501f3bd0
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-11-22 20:26:41 +0100

    implemented chat bot with gpt

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,7 @@ import userloginController from "../controllers/user.login.js"
 import authUser from '../middleware/auth.user.js';
 import produceController from '../controllers/produce.controller.js';
 // import ensureOwnership from '../middleware/farmers.auth.js';
+import chatController from '../controllers/chat.control.js';
 
 const router = express.Router();
 
@@ -17,6 +18,10 @@ router.post("/produce/create", authUser, produceController.addProduce)
 router.get('/produce/list/:userId', authUser, produceController.getProduce);
 router.put('/produce/update/:userId/:produceId', authUser, produceController.updateProduce);
 router.delete('/produce/delete/:userId/:produceId', authUser, produceController.deleteProduce);
+
+// chat route
+router.post("/chat", chatController.chat)
+
 
 
 export { router };
